160a60c3ad247adce1bf9e825e5c4f4fe7ccbf14
felipemz <juan.mendez@bolsiyo.app>
2025-05-16 22:20:11 -0500

    ui

--- app/src/main/java/com/coordinadora/technicaltest/di/AppComponent.java
+++ app/src/main/java/com/coordinadora/technicaltest/di/AppComponent.java
@@ -1,5 +1,6 @@
 package com.coordinadora.technicaltest.di;
 
+import com.coordinadora.technicaltest.ui.login.LoginActivity;
 import com.coordinadora.technicaltest.ui.main.MainActivity;
 
 import javax.inject.Singleton;
@@ -10,4 +11,5 @@ import dagger.Component;
 @Component(modules = {AppModule.class})
 public interface AppComponent {
     void inject(MainActivity activity);
+    void inject(LoginActivity activity);
 }
---
+++ app/src/main/java/com/coordinadora/technicaltest/ui/login/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.coordinadora.technicaltest.ui.login;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.coordinadora.technicaltest.R;
+import com.coordinadora.technicaltest.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
